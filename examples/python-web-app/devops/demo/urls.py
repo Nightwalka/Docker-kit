3a43cd6afe674b451852d09b449f0eada7eec453
Nightwalka <nightwalkaa@outlook.com>
2025-04-25 20:55:38 +0800

    changes

--- examples/python-web-app/devops/demo/urls.py
+++ examples/python-web-app/devops/demo/urls.py
@@ -1,7 +1,7 @@
-from django.urls import path
-
-from . import views
+from django.contrib import admin
+from django.urls import path, include  # <-- include is needed
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('admin/', admin.site.urls),
+    path('', include('demo.urls')),   # <-- THIS is the fix
 ]
